4e27f39b75ea1e8bd1f8c2a39159a6eee14a5d69
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-04-04 21:42:36 -0500

    added api routes and started up database

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post.Routes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeroutes');
-const apiRoutes = require("./api")
-// const login = require("./homeroutes")
-// const contact = require("./homeroutes")
-// const cart = require("./homeroutes")
-//catalog contact sell
+// const apiRoutes = require("./api")
+
 
 router.use('/', homeRoutes);
-router.use("/api", apiRoutes)
-// router.use("/sell", sell);
-// router.use("/contact", contact);
-// router.use("/cart", cart);
+// router.use("/api", apiRoutes)
+
 
 router.use((req, res) => {
   res.send('<h1>Wrong Route!</h1>');
