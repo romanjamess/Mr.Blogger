79c4eb1c52d7f977e0fe69a920a8c827e2391ac1
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-04-13 03:54:19 -0500

    comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post.Routes');
-const commentRoutes = require('./commentRoutes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
